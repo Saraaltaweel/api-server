f54033b39916c3088aa1693118eedbddaccfc002
Saraaltaweel <s.bata97.st@gmail.com>
2021-06-30 21:25:24 +0300

    done

---
+++ __tests__/logger.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const logger=require('../src/middleware/logger');
+
+describe('logger middleware',()=>{
+    let consoleSpy;
+    let req={};
+    let res={};
+    let next=jest.fn();
+
+    beforeEach(()=>{
+        consoleSpy=jest.spyOn(console,'log').mockImplementation();
+    })
+
+    afterEach(()=>{
+        consoleSpy.mockRestore();
+    })
+
+    it('logs the output',()=>{
+        logger(req,res,next);
+        expect(consoleSpy).toHaveBeenCalled();
+        expect(next).toHaveBeenCalled();
+    })
+
+})
